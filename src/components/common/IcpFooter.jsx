d73ad6b0733e7a227fd71252499aafff170a75d4
will zhang <zhangdzh2010@gmail.com>
2026-05-27 16:22:03 +0800

    fix: link ICP footer to MIIT

--- src/components/common/IcpFooter.jsx
+++ src/components/common/IcpFooter.jsx
@@ -2,14 +2,26 @@ import { View, Text } from '@tarojs/components';
 import { isH5Runtime } from '../../utils/runtime';
 import './IcpFooter.scss';
 
+const ICP_RECORD_URL = 'https://beian.miit.gov.cn/';
+
 export function IcpFooter() {
   if (!isH5Runtime()) {
     return null;
   }
 
+  const openIcpRecord = () => {
+    if (typeof window === 'undefined') {
+      return;
+    }
+
+    window.open(ICP_RECORD_URL, '_blank', 'noopener,noreferrer');
+  };
+
   return (
     <View className="icp-footer">
-      <Text className="icp-footer__text">粤ICP备2026008980号</Text>
+      <Text className="icp-footer__text" onClick={openIcpRecord}>
+        粤ICP备2026008980号
+      </Text>
     </View>
   );
 }
